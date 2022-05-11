3014e0016f0722839679cbab1ccd82ce5cff183c
ngohieuphat <ngohipha1@gmail.com>
2022-05-11 21:44:53 +0700

    margin , padding

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+//state object
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
+    print("build");
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Page"),
+        title: Text("Home Page"),
       ),
       body: Container(
-        child: const Text("Home page Screen"),
-      ),
+          constraints: BoxConstraints.expand(),
+          color: Colors.blue,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(color: Colors.red, child: Text("ABC")),
+          )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
-
-
-
